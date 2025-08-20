6bce9162a556db27687967c8acde4004c5b24d9a
Rodney Etienne <czarsuperstar@gmail.com>
2025-08-20 08:10:45 -0400

    pyxis: Initialize for Evolution X 10.x

--- lineage_pyxis.mk
+++ lineage_pyxis.mk
@@ -11,6 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_USES_MINI_GAPPS := true
+WITH_GMS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_DISABLE_EPPE := true
+
 # Inherit from pyxis device
 $(call inherit-product, device/xiaomi/pyxis/device.mk)
 
